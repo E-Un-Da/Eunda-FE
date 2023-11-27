7e0860618528b5149bd05eb10fbbcd24d201bf52
hongui <honge7694@naver.com>
2023-11-28 03:01:47 +0900

    Animation 만들기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
 import LoginUserHome from './LoginUserHome';
+import Motion from '../framer/Motion';
+
 
 const Home = () => {
 	return <>
+		< Motion />
 		{ LoginUserHome && <LoginUserHome /> }
 	</>
 }
