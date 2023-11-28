ef2954b9abc342bef8f11fd29a90d258587d456e
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-28 14:01:38 +0900

    Home 화면 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React from 'react';
 import LoginUserHome from './LoginUserHome';
 import Motion from '../framer/Motion';
-
+import '../styles/home.css';
 
 const Home = () => {
-	return <>
-		< Motion />
-		{ LoginUserHome && <LoginUserHome /> }
-	</>
-}
+  return (
+    <>
+      {/* < Motion /> */}
+      <div class='container'>
+        <div class='dropping-texts'>
+          <div>Spring</div>
+          <div>React</div>
+          <div>Node</div>
+          <div>E-UN-DA!</div>
+        </div>
+      </div>
+      {LoginUserHome && <LoginUserHome />}
+    </>
+  );
+};
 
 export default Home;
--- src/pages/LoginUserHome.js
+++ src/pages/LoginUserHome.js
@@ -4,89 +4,110 @@ import { Card, Carousel } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
-
 const { Meta } = Card;
 
 const LoginUserHome = () => {
-    const carouselRef = useRef(null);
-    const history = useNavigate();
-    const [myStudies, setMyStudies] = useState();
-    
-    useEffect(() => {
-      const myStudies = async () => {
-        try {
-          const response = await axios.get(
-            "http://localhost:8080/studies/my-studies"
-          );
-          console.log("myStudies response : ", response);
-          setMyStudies(response.data);
-          console.log(response.data[0]);              
-        } catch (error) {
-          console.log(error);
-        }
+  const carouselRef = useRef(null);
+  const history = useNavigate();
+  const [myStudies, setMyStudies] = useState();
+
+  useEffect(() => {
+    const myStudies = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:8080/studies/my-studies'
+        );
+        console.log('myStudies response : ', response);
+        setMyStudies(response.data);
+        console.log(response.data[0]);
+      } catch (error) {
+        console.log(error);
       }
-      myStudies();
-    }, []);
-        
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-    };    
-    
-    const handlerOnClick = (e, id) => {
-      e.preventDefault();
-      console.log(id);
-      
-      history('/studies/'+id);
     };
-    
-    const handlePrev = () => {
-        carouselRef.current.prev();
-    };
+    myStudies();
+  }, []);
 
-    const handleNext = () => {
-        carouselRef.current.next();
-    };
-    
-    return (
-        <div style={{ 
-            padding: '20px',  
-            maxWidth: '1300px',
-            margin: '0 auto',  // 수평 가운데 정렬
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
+
+  const handlerOnClick = (e, id) => {
+    e.preventDefault();
+    console.log(id);
+
+    history('/studies/' + id);
+  };
+
+  const handlePrev = () => {
+    carouselRef.current.prev();
+  };
+
+  const handleNext = () => {
+    carouselRef.current.next();
+  };
+
+  return (
+    <div
+      style={{
+        padding: '20px',
+        maxWidth: '1300px',
+        margin: '0 auto', // 수평 가운데 정렬
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '50px',
+      }}
+    >
+      <h1 style={{ marginBottom: '15px' }}>My Studies</h1>
+      <Card>
+        <Carousel ref={carouselRef} {...settings}>
+          {myStudies &&
+            myStudies.map((study) => (
+              <a
+                href='#'
+                style={{ textDecoration: 'none', color: 'black' }}
+                onClick={(e) => handlerOnClick(e, study.id)}
+              >
+                <div key={study.id} style={{ color: 'black' }}>
+                  <Meta title={study.title} description={study.intro} />
+                  <p style={{ marginTop: '8px', marginBottom: '8px' }}>
+                    카테고리: {study.category}
+                  </p>
+                  <p style={{ marginBottom: '8px' }}>
+                    스터디장: {study.leader}
+                  </p>
+                  <p style={{ marginBottom: '8px' }}>
+                    현재 인원: {study.headcount}
+                  </p>
+                  <p style={{ marginBottom: '8px' }}>
+                    모집 인원: {study.recruitNum}
+                  </p>
+                </div>
+              </a>
+            ))}
+        </Carousel>
+        <div
+          style={{
             display: 'flex',
-            flexDirection: 'column',
-            marginTop: '50px',
-        }}>
-            <h1 style={{marginBottom: "15px"}}>My Studies</h1>
-          <Card >
-            <Carousel ref={ carouselRef } {...settings}>
-              {myStudies && myStudies.map((study) => (
-                <a href="#" style={{ textDecoration: "none",  color: "black" }} onClick={ (e) => handlerOnClick(e, study.id) }>
-                  <div key={study.id} style={{color: "black"}}>
-                    <Meta
-                      title={study.title}
-                      description={study.intro}
-                    />
-                      <p style={{ marginTop: "8px", marginBottom: '8px' }}>Category: {study.category}</p>
-                      <p style={{ marginBottom: '8px' }}>Leader: {study.leader}</p>
-                      <p style={{ marginBottom: '8px' }}>Headcount: {study.headcount}</p>
-                      <p style={{ marginBottom: '8px' }}>Recruit Num: {study.recruitNum}</p>
-                      <p style={{ marginBottom: '8px' }}>Recruit: {study.recruit ? 'Yes' : 'No'}</p>
-
-                  </div>
-                </a>
-              ))}
-            </Carousel>
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-                <LeftOutlined style={{ fontSize: '24px', cursor: 'pointer' }} onClick={handlePrev} />
-                <RightOutlined style={{ fontSize: '24px', cursor: 'pointer' }} onClick={handleNext} />
-            </div>
-          </Card>
+            justifyContent: 'center',
+            marginTop: '10px',
+          }}
+        >
+          <LeftOutlined
+            style={{ fontSize: '24px', cursor: 'pointer' }}
+            onClick={handlePrev}
+          />
+          <RightOutlined
+            style={{ fontSize: '24px', cursor: 'pointer' }}
+            onClick={handleNext}
+          />
         </div>
-      );
-}
+      </Card>
+    </div>
+  );
+};
 
 export default LoginUserHome;
